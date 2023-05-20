c2caa648d4964d68050352d270fe2f25c9c5444a
artsmilej <artemus200918@gmail.com>
2023-05-20 14:26:44 +0300

    Add designHangman

---
+++ DesignHangman.cpp
@@ -0,0 +1,149 @@
+﻿#include "DesignHangman.h"
+#include "DesignDefine.h"
+#include <iostream>
+#include <io.h>
+#include <fcntl.h>
+
+void DesignHangman::HangmanStageStart() {
+   
+    std::wcout
+        << DGR << L"      ╭──\n" << WHT
+        << DGR << L"      │╱\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageOne() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageTwo() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageThree() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << YEL << L"        O\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageFour() {
+
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << YEL << L"        O\n"
+        << DGR << L"      │  " << GRN << L"        │\n"
+        << DGR << L"      │  " << GRN << L"        │\n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageFive() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << YEL << L"        O\n"
+        << DGR << L"      │  " << RED << L"       ╱" << GRN << L"│\n"
+        << DGR << L"      │  " << GRN << L"        │ \n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageSix() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << YEL << L"        O\n"
+        << DGR << L"      │  " << RED << L"       ╱" << GRN << L"│" << MAG << L"╲ \n"
+        << DGR << L"      │  " << GRN << L"        │ \n"
+        << DGR << L"      │\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageSeven() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << YEL << L"        O\n"
+        << DGR << L"      │  " << RED << L"       ╱" << GRN << L"│" << MAG << L"╲ \n"
+        << DGR << L"      │  " << GRN << L"        │ \n"
+        << DGR << L"      │  " << BLU << L"       ╱\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
+
+void DesignHangman::HangmanStageEight() {
+    
+    std::wcout
+        << DGR << L"      ╭──────────┬───\n" << WHT
+        << DGR << L"      │╱ " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << WHT << L"        ┆\n"
+        << DGR << L"      │  " << YEL << L"        O\n"
+        << DGR << L"      │  " << RED << L"       ╱" << GRN << L"│" << MAG << L"╲ \n"
+        << DGR << L"      │  " << GRN << L"        │ \n"
+        << DGR << L"      │  " << BLU << L"       ╱" << CYN << L" ╲\n"
+        << DGR << L"      │\n"
+        << L"      │\n"
+        << L"      │\n"
+        << L"    ══╧══════════════\n" << WHT;
+}
---
+++ DesignHangman.h
@@ -0,0 +1,14 @@
+#pragma once
+class DesignHangman {
+public:
+	void HangmanStageStart();
+	void HangmanStageOne();
+	void HangmanStageTwo();
+	void HangmanStageThree();
+	void HangmanStageFour();
+	void HangmanStageFive();
+	void HangmanStageSix();
+	void HangmanStageSeven();
+	void HangmanStageEight();
+};
+
